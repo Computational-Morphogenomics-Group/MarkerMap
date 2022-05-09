0df8a3bf5db186dd6cb3f45c8393ff9516e6faa8
Wilson Gregory <wilson.g.gregory@gmail.com>
2022-05-09 15:35:02 -0400

    updates

--- scripts/diffusion_map.py
+++ scripts/diffusion_map.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scanpy as sc
 import math
+import time
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.decomposition import PCA
@@ -14,6 +15,17 @@ from torch import nn
 from pydiffmap import diffusion_map, visualization
 from markermap.utils import new_model_metrics, parse_adata, RandomBaseline, SmashPyWrapper, split_data_into_dataloaders, form_block
 
+
+def getMattSet():
+    X = np.random.multivariate_normal([0,0],[[0.75,0],[0,0.75]],300)
+    y = []
+    while len(y) < 300:
+        k = np.random.multivariate_normal([0,0],[[20,0],[0,20]]).tolist()
+        if k[0]**2 + k[1]**2 <= 7 or k[0]**2 >= 10:
+            continue
+        else:
+            y.append(k)
+    y = np.array(y)
 
 def getSwissRoll():
     # set parameters
@@ -41,15 +53,16 @@ def getSwissRoll():
     mydmap = diffusion_map.DiffusionMap.from_sklearn(n_evecs=n_evecs, k=100, epsilon=0.0156, alpha=1.0, neighbor_params=neighbor_params)
     # fit to data and return the diffusion map.
     mydmap.fit(swiss_roll)
-    print(mydmap.epsilon_fitted)
 
     # plot the diffusion embeddings
-    plot2d(mydmap.dmap, n_cols=n_evecs, colors=mydmap.dmap[:,0], show=False)
+    # plot2d(mydmap.dmap, n_cols=n_evecs, colors=mydmap.dmap[:,0], show=False)
 
     # plot the data in the original dimensions
-    plot3d(mydmap.data, n_cols=3, colors=mydmap.dmap[:,0], show=True)
+    # plot3d(mydmap.data, n_cols=3, colors=mydmap.dmap[:,0], show=True)
 
-    return swiss_roll
+    return swiss_roll, mydmap.dmap[:,0], 0.0156, 100
+
+    # return swiss_roll
 
 
 def plot2d(X, n_cols, skip_eigenvecs = {}, colors = None, show=True):
@@ -75,6 +88,8 @@ def plot2d(X, n_cols, skip_eigenvecs = {}, colors = None, show=True):
 
             ax1.set_xlabel(f'Eigenvec {i}')
             ax1.set_ylabel(f'Eigenvec {j}')
+
+            # fig1.savefig(f'/Users/wilson/Documents/jhu/spring_2022/highD_approx/final_project/citeseq_evec_pairs/citeseq_evecs_{i}_{j}')
 
     plt.tight_layout()
 
@@ -129,6 +144,7 @@ def getCiteSeqData():
     adata = sc.read_h5ad('data/cite_seq/CITEseq.h5ad')
     adata.obs['annotation'] = adata.obs['names']
     X, y, encoder = parse_adata(adata)
+    print(X.shape)
 
     # remove some outliers
     outliers = [1066, 779, 8458]
@@ -136,8 +152,9 @@ def getCiteSeqData():
     y = np.delete(y, outliers)
 
     # epsilon = 64 # given by BGH
-    epsilon = X.shape[1]/5
-    k = 100
+    # epsilon = X.shape[1]/5
+    epsilon = 16
+    k = 25
 
     return X, y, epsilon, k
 
@@ -188,65 +205,103 @@ def getPaulData():
     return X, y, epsilon, k
 
 
-def runDiffMapAndPlotPairs(X, y, epsilon, k, max_plots=7, eval_models = None):
-    _, _, _, train_indices, _, test_indices = split_data_into_dataloaders(X,y, 0.8, 0) # train 80%, val 0%, test 20%
+def getSkreePlots(X, y, epsilon, k, n_evecs):
+    mydmap = diffusion_map.DiffusionMap.from_sklearn(n_evecs = n_evecs, epsilon=epsilon, alpha=1, k=k)
+    mydmap.fit(X)
+
+    pca = PCA(n_components=np.min([n_evecs, X.shape[1]]))
+    X_pca = pca.fit_transform(X)
+
+    diff_map_evals = np.sqrt(-1. / mydmap.evals)
+
+    fig1, ax1 = plt.subplots()
+    ax1.plot(range(n_evecs), diff_map_evals[:n_evecs], marker='o')
+    ax1.set_title('Diffusion Map Eigenvalues')
+
+    print(pca.explained_variance_)
+
+    fig2, ax2 = plt.subplots()
+    ax2.plot(range(len(pca.explained_variance_)), pca.explained_variance_, marker='o')
+    ax2.set_title('PCA Eigenvalue')
+    plt.show()
+
+
+
+def evaluateDiffMapClassification(X, y, epsilon, k, eval_models, num_times=1):
     n_evecs = len(np.unique(y))
 
-    if not isinstance(eval_models, list):
-        eval_models = [eval_models]
+    mydmap = diffusion_map.DiffusionMap.from_sklearn(n_evecs = n_evecs, epsilon=epsilon, alpha=1, k=k)
+    mydmap.fit(X)
 
-    for eval_model in eval_models:
-        print(type(eval_model))
+    pca = PCA(n_components=n_evecs)
+    X_pca = pca.fit_transform(X)
 
-        # Diffusion map eval
-        mydmap = diffusion_map.DiffusionMap.from_sklearn(n_evecs = n_evecs, epsilon=epsilon, alpha=1, k=k)
-        mydmap.fit(X)
-        misclass_rate, _, _ = new_model_metrics(
-            mydmap.dmap[train_indices, :],
-            y[train_indices],
-            mydmap.dmap[test_indices, :],
-            y[test_indices],
-            model = eval_model,
-        )
-        print(misclass_rate)
+    results = { key: np.zeros((3,num_times)) for key in eval_models.keys() }
+    for i in range(num_times):
+        _, _, _, train_indices, _, test_indices = split_data_into_dataloaders(X,y, 0.8, 0) # train 80%, val 0%, test 20%
+        print(i)
 
-        # All original features baseline
-        baseline_misclass, _, _ = new_model_metrics(
-            X[train_indices, :],
-            y[train_indices],
-            X[test_indices, :],
-            y[test_indices],
-            model = eval_model,
-        )
-        print(baseline_misclass)
+        for model_label, eval_model in eval_models.items():
 
-        # PCA baseline
-        pca = PCA(n_components=n_evecs)
-        X_pca = pca.fit_transform(X)
-        pca_misclass, _, _ = new_model_metrics(
-            X_pca[train_indices, :],
-            y[train_indices],
-            X_pca[test_indices, :],
-            y[test_indices],
-            model = eval_model,
-        )
-        print(pca_misclass)
+            # Diffusion map eval
+            misclass_rate, _, _ = new_model_metrics(
+                mydmap.dmap[train_indices, :],
+                y[train_indices],
+                mydmap.dmap[test_indices, :],
+                y[test_indices],
+                model = eval_model,
+            )
+            results[model_label][0,i] = misclass_rate
+
+            # All original features baseline
+            baseline_misclass, _, _ = new_model_metrics(
+                X[train_indices, :],
+                y[train_indices],
+                X[test_indices, :],
+                y[test_indices],
+                model = eval_model,
+            )
+            results[model_label][1,i] = baseline_misclass
+
+            # PCA baseline
+            pca_misclass, _, _ = new_model_metrics(
+                X_pca[train_indices, :],
+                y[train_indices],
+                X_pca[test_indices, :],
+                y[test_indices],
+                model = eval_model,
+            )
+            results[model_label][2,i] = pca_misclass
+
+    for k,v in results.items():
+        print(k, np.mean(results[k], axis=1))
+
+
+def runDiffMapAndPlotPairs(X, y, epsilon, k, max_plots=7):
+    n_evecs = len(np.unique(y))
+
+    mydmap = diffusion_map.DiffusionMap.from_sklearn(n_evecs = n_evecs, epsilon=epsilon, alpha=1, k=k)
+    mydmap.fit(X)
 
     # plot the diffusion embeddings
     plot2d(mydmap.dmap, n_cols=np.min([n_evecs, max_plots]), colors=y, show=True)
 
 
-def findDiffMapKValue(X, y, epsilon, k_range):
+def findDiffMapKValue(X, y, epsilon, k_range, display_time=False):
     _, _, _, train_indices, _, test_indices = split_data_into_dataloaders(X,y, 0.8, 0) # train 80%, val 0%, test 20%
     # test for different values of k
     n_evecs = len(np.unique(y))
 
-    misclass_rates = { 'diff_map': [], 'pca': [] }
+    misclass_rates = []
+    runtime = []
     for k in k_range:
         print('k:', k)
 
         mydmap = diffusion_map.DiffusionMap.from_sklearn(n_evecs = n_evecs, epsilon=epsilon, alpha=1, k=k)
+        start_time = time.time()
         mydmap.fit(X)
+        end_time = time.time() - start_time
+        runtime.append(end_time)
 
         misclass_rate, _, _ = new_model_metrics(
             mydmap.dmap[train_indices, :],
@@ -255,68 +310,63 @@ def findDiffMapKValue(X, y, epsilon, k_range):
             y[test_indices],
             # markers=n_evecs,
         )
-        misclass_rates['diff_map'].append(misclass_rate)
-
-        # PCA baseline
-        pca = PCA(n_components=np.min([k,X.shape[1]]))
-        X_pca = pca.fit_transform(X)
-        pca_misclass, _, _ = new_model_metrics(
-            X_pca[train_indices, :],
-            y[train_indices],
-            X_pca[test_indices, :],
-            y[test_indices],
-        )
-        misclass_rates['pca'].append(pca_misclass)
-
-        # mydmap = diffusion_map.DiffusionMap.from_sklearn(n_evecs = n_evecs, epsilon=epsilon, alpha=0.5, k=k)
-        # mydmap.fit(X)
-
-        # misclass_rate, _, _ = new_model_metrics(
-        #     mydmap.dmap[train_indices, :],
-        #     y[train_indices],
-        #     mydmap.dmap[test_indices, :],
-        #     y[test_indices],
-        #     # markers=n_evecs,
-        # )
-        # misclass_rates['alpha2'].append(misclass_rate)
+        misclass_rates.append(misclass_rate)
 
     baseline_misclass, _, _ = new_model_metrics(X[train_indices, :], y[train_indices], X[test_indices, :], y[test_indices])
 
+    # PCA baseline
+    pca = PCA(n_components=n_evecs)
+    X_pca = pca.fit_transform(X)
+    pca_misclass, _, _ = new_model_metrics(
+        X_pca[train_indices, :],
+        y[train_indices],
+        X_pca[test_indices, :],
+        y[test_indices],
+    )
+
     fig1, ax1 = plt.subplots()
-    ax1.scatter(k_range, misclass_rates['diff_map'], label='diff_map')
-    ax1.scatter(k_range, misclass_rates['pca'], label='pca')
-    ax1.hlines(baseline_misclass, k_range[0], k_range[-1])
+    ax1.plot(k_range, misclass_rates, label='diff_map', marker='o')
+    ax1.hlines(baseline_misclass, k_range[0], k_range[-1], label='baseline', colors=['red'])
+    ax1.hlines(pca_misclass, k_range[0], k_range[-1], label='pca', colors=['orange'])
     ax1.legend()
+
+    if display_time:
+        fig2, ax2 = plt.subplots()
+        ax2.plot(k_range, runtime, label='runtime', marker='o')
+        ax2.legend()
+
     plt.tight_layout()
     plt.show()
 
-def makeNeuralNetworkClassifier(input_size, hidden_size, output_size):
-    classifier = nn.Sequential(
-        *form_block(input_size, hidden_size, bias = bias, batch_norm = True),
-        nn.Linear(hidden_size, output_size, bias = bias),
-    )
-
-
-
 
 # MAIN
-# eval_model = None
-eval_model = [KNeighborsClassifier(), RandomForestClassifier(), MLPClassifier(max_iter=1000)]
-
 # Zeisel
-k_range = [5,10,15,20,25,30,50,100]
-X, y, epsilon, k = getZeiselData()
+# k_range = [5,10,20,30,50,100,200,500,1000,2000,3000]
+# X, y, epsilon, k = getZeiselData()
 
 # # Cite-Seq
-# k_range = [20,30,50,100,200,500,1000,3000,5000,8000]
-# X, y, epsilon, k = getCiteSeqData()
+k_range = [5,10,20,30,50,100,200,500,1000,3000,5000,8000]
+X, y, epsilon, k = getCiteSeqData()
 
 # Paul
 # k_range = [15,20,25,30,50,100,200,300,400,500]
 # X, y, epsilon, k = getPaulData()
 
-runDiffMapAndPlotPairs(X, y, epsilon, k, max_plots=2, eval_models=eval_model)
+# Swiss Roll
+# X, y, epsilon, k = getSwissRoll()
 
-# findDiffMapKValue(X, y, epsilon, k_range)
+eval_models = {
+    'knn': KNeighborsClassifier(),
+    # 'rf': RandomForestClassifier(),
+    # 'neural network': MLPClassifier(max_iter=1000),
+}
+
+# runDiffMapAndPlotPairs(X, y, epsilon, k, max_plots=7)
+
+evaluateDiffMapClassification(X, y, epsilon, k, num_times=1, eval_models=eval_models)
+
+# findDiffMapKValue(X, y, epsilon, k_range, display_time=True)
+
+# getSkreePlots(X, y, epsilon, k, n_evecs=100)
 
 
